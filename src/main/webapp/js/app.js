cbffc58d8f942dc4d565df212dd4cd9bebce463c
kirankrkumar <kiran05172016@gmail.com>
2016-06-13 09:55:52 -0400

    Some small chnges

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -3,7 +3,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('loginApp', ['loginApp.filters', 'loginApp.services', 'loginApp.directives', 'loginApp.controllers']).
     config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view1', {templateUrl: 'partials/login.html', controller: 'loginCtrl'});
-        $routeProvider.when('/landingPage', {templateUrl: 'partials/userDashBoard.html', controller: 'userDashBoradCtrl'});
-        $routeProvider.otherwise({redirectTo: '/view1'});
+        $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'loginCtrl'});
+        $routeProvider.when('/userDashBoard', {templateUrl: 'partials/userDashBoard.html', controller: 'userDashBoradCtrl'});
+        $routeProvider.otherwise({redirectTo: '/login'});
     }]);
