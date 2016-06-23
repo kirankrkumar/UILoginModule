f54c934d925205b6fb5f706629793032d0e21eb8
kirankrkumar <kiran05172016@gmail.com>
2016-06-23 14:29:46 -0400

    Updated UserFactory and Removed UserAuth,UserRoles Service Factories

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -4,31 +4,23 @@
 
 var services = angular.module('loginApp.services', ['ngResource']);
 
-services.factory('UserFactory', function ($resource) {
-    return $resource('/midLayerloginModule/rest/users', {}, {
-        query: {
-            method: 'GET',
-            params: {},
-            isArray: false
-        }
-    })
-    
-   
-});
-
-services.factory('UserRoles', function ($resource) {
-    return $resource('/midLayerloginModule/rest/users/roles', {}, {
-        query: {
-            method: 'GET',
-            params: {},
-            isArray: true
-        }
-    })
-    
-   
-});
-
-services.factory('UserAuth', function ($resource) {
-	console.log("it is from loginModule call in serice js:::::");
-     return $resource('/midLayerloginModule/rest/users/auth');
-});
+services.factory('UserFactory', ['$resource', function ($resource) {
+    //$resource() function returns an object of resource class.
+	var Url = 'http://localhost\\:8082/SpringRestfulHelloWorldExample/user/signup';
+	
+    return $resource(
+            Url, 
+            {},//parameters
+            {
+                update: {
+                      method: 'PUT' // To send the HTTP Put request when calling this custom update method.
+                },
+                get: {
+                      method: 'GET', cache: true // To cache the GET request fired using 'get' method.
+                }
+            }, 
+            {
+                stripTrailingSlashes: false
+            }
+    );
+}]);
