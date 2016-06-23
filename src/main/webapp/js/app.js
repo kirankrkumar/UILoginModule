ec6090caaa74774eb15dc99377de0af1e38b9689
kirankrkumar <kiran05172016@gmail.com>
2016-06-23 14:13:28 -0400

    Added CORS(Cross Origin Resource Sharing) in app.config

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,9 +1,13 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('loginApp', ['loginApp.filters', 'loginApp.services', 'loginApp.directives', 'loginApp.controllers']).
-    config(['$routeProvider', function ($routeProvider) {
+angular.module('loginApp', ['loginApp.filters', 'loginApp.services', 'loginApp.directives', 'loginApp.controllers', 'ngCookies']).
+    config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
         $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'loginCtrl'});
+        $routeProvider.when('/signup', {templateUrl: 'partials/signup.html', controller: 'signupCtrl'});
         $routeProvider.when('/userDashBoard', {templateUrl: 'partials/userDashBoard.html', controller: 'userDashBoradCtrl'});
         $routeProvider.otherwise({redirectTo: '/login'});
+        
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
     }]);
